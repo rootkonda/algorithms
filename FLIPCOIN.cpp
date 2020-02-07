9ce0ff2d6f9a21391f74e10f2e8533f4fc0c08fb
konda89 <52867909+rootkonda@users.noreply.github.com>
2020-02-08 00:54:48 +0100

    FLIPCOIN

--- FLIPCOIN.cpp
+++ FLIPCOIN.cpp
@@ -1,41 +1,111 @@
 #include <iostream>
+#include <math.h>
 
-int main()
+typedef long long ll;
+
+ll st[1000000]={0};
+ll lazy[1000000]={0};
+
+inline ll GetMid(ll a, ll b)
 {
-    int n,q,cmd[3],count;
-    int res[100001][100001]={-1};
+    return a+(b-a)/2;
+}
 
-    scanf("%d %d",&n,&q);
 
-    int a[100001]={};
+ll HeadCount(ll ss,ll se,ll qs,ll qe,ll si)
+{
+    ll mid;
+    ll left,right;
+    
+    if(lazy[si]==1)
+    {
+        st[si]=(se-ss)+1-st[si];
+        if(ss!=se)
+        {
+            lazy[(2*si)+1]=1-lazy[(2*si)+1];
+            lazy[(2*si)+2]=1-lazy[(2*si)+2];
+        }
+        lazy[si]=0;
+    }
+    
+    if(ss >qe || se <qs)
+        return 0;
+    
+    if(ss>=qs && se<=qe)
+    {
+        return st[si];
+    }
+    mid=GetMid(ss, se);
+    left=HeadCount(ss, mid, qs, qe, (2*si)+1);
+    right=HeadCount(mid+1, se, qs, qe, (2*si)+2);
+    
+    return left+right;
+   
+}
 
-    while(q-- >0)
+ll FlipCoin(ll ss,ll se,ll qs,ll qe,ll si)
+{
+    ll mid;
+    
+    if(lazy[si]==1)
+    {
+        st[si]=(se-ss)+1-st[si];
+        if(ss!=se)
+        {
+            lazy[(2*si)+1]=1-lazy[(2*si)+1];
+            lazy[(2*si)+2]=1-lazy[(2*si)+2];
+        }
+        lazy[si]=0;
+    }
+    
+    if(ss >qe || se <qs)
+        return st[si];
+        
+    if(ss>=qs && se<=qe)
+    {
+        st[si]=((se-ss)+1)-st[si];
+        if(se!=ss)
+        {
+            lazy[(2*si)+1]=1-lazy[(2*si)+1];
+            lazy[(2*si)+2]=1-lazy[(2*si)+2];
+        }
+        
+        return st[si] ;
+    }
+    
+    
+    mid=GetMid(ss, se);
+    ll left=FlipCoin(ss, mid, qs, qe, (2*si)+1);
+    ll right=FlipCoin(mid+1, se, qs, qe, (2*si)+2);
+    
+    st[si]=left+right;
+    return st[si];
+    
+}
+    
+
+
+int main(int argc, const char * argv[]) {
+
+    int t,n,cmd[3];
+    
+    scanf("%d %d",&n,&t);
+    
+    while(t-- > 0)
     {
         for(int i=0;i<3;i++)
             scanf("%d",&cmd[i]);
-        
+
         if(cmd[0]==0)
         {
-            count=0;
-            for(int j=cmd[1];j<=cmd[2];j++)
-            {  
-                 a[j]=(a[j]==0 ? 1 : 0);
-                 if(a[j]==1)
-                    res[j][cmd[2]]=count++;
-            }
+            FlipCoin(0, n-1, cmd[1], cmd[2], 0);
         }
         else
         {
-            count=0;
-            for(int k=cmd[1];k<=cmd[2];k++)
-            {
-                if(a[k]==1)
-                    count++;
-            }
-            printf("%d\n",count);
-            res[cmd[1]][cmd[2]]=count;
+            printf("%llu \n",HeadCount(0, n-1, cmd[1], cmd[2], 0));
         }
-    }
 
+    }
+    
     return 0;
 }
